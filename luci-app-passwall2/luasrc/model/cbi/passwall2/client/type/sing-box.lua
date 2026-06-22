b64e2f33c25e96502db0f8c6bbcb212415760d9a
A-BO <86697442+lwb1978@users.noreply.github.com>
2026-06-22 17:51:09 +0800

    luci: add `vivo` connection detection url option

--- luci-app-passwall2/luasrc/model/cbi/passwall2/client/type/sing-box.lua
+++ luci-app-passwall2/luasrc/model/cbi/passwall2/client/type/sing-box.lua
@@ -165,7 +165,8 @@ if load_urltest_options then -- [[ URLTest Start ]]
 	o:value("https://www.youtube.com/generate_204", "YouTube")
 	o:value("https://connect.rom.miui.com/generate_204", "MIUI (CN)")
 	o:value("https://connectivitycheck.platform.hicloud.com/generate_204", "HiCloud (CN)")
-	o.default = "https://www.gstatic.com/generate_204"
+	o:value("https://wifi.vivo.com.cn/generate_204", "VIVO (CN)")
+	o.default = o.keylist[3]
 	o.description = translate("The URL used to detect the connection status.")
 
 	o = s:option(Value, _n("urltest_interval"), translate("Test interval"))
